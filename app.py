7384734071a9c8c499d0377603850b0058653da4
mateussiilva <mateuspython3010@gmail.com>
2023-10-12 23:09:17 -0300

    Refatorando as funções de manipulação de html

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from pprint import pprint as print
+import PySimpleGUI as sg
+
+
+layout = [
+    
+]
 
 
 
-dados_impressoes = ler_dados_json("arquivos_json/RIPLOG.json")
-
-        
-
-utl.gravar_txt(gravar_informações_uteis(dados_impressoes),"teste.txt")
+# MAIN
+while True:
+    
--- backend.py
+++ backend.py
@@ -5,33 +5,40 @@ import glob
 
 FOLDER_JSON_FILES = "json_files"
 
+PLOTTERS = {
+    "mutoh":"1604",
+    "prismajet":"1602",
+    "prismatex":"1904"}
+
+PLOTTER = PLOTTERS["prismatex"]
 
 def create_new_name(path):
     nome, e = os.path.split(path)
     n, exte = os.path.splitext(e)
     return n+".json"
 
+def get_files_htmls(path,extension="*.HTML") -> set:
+    list_files_htmls = set()
+    for file_html in glob.glob(os.path.join(path,extension)):
+        list_files_htmls.add(file_html)
+        
+    return list_files_htmls
+        
+    
     
 def main():
-    PATH = "testes/1904/09 23"
-    EXTE = "*.HTML"
-    for file_html in glob.glob(os.path.join(PATH,EXTE)):
-        print(create_new_name(file_html))
-        
+    PATH = "testes/1904/08 23"
+    files_htmls = get_files_htmls(PATH)
 
-
-
-
+    path_json_file = os.path.join(FOLDER_JSON_FILES,PLOTTER,create_new_name(file_html))
+    contexto = pyhtml.create_context_html(file_html)
+    lista_dados = pyhtml.struct_base_file(contexto, "table")
+    dicionario = pyhtml.create_dict_dados(lista_dados)
+    
+    write_json_file(
+        path_json_file,dados=dicionario
+    )        
+    dicionario.clear()
 if __name__ == "__main__":
     main()
-    # path_html_file = "gerenciador_de_log/arquivos_htmls/29 08 23.HTML"
-    # dicionario = {"matesu":"jose"}
-    # write_json_file(os.path.join(FOLDER_JSON_FILES,create_new_name(path_html_file)),dicionario)
-    
-    # content_html = pyhtml.create_context_html(path_html_file)
-    # list_dados = pyhtml.struct_base_file(content_html,"table")
-    # dicinarios_limpos = pyhtml.create_dict_dados(list_dados)
-    # for dados in dicinarios_limpos:
-    #     print(dados)
-
 
