89fd19f7f210ac21beed8a19329fdc4fd1d2ddc5
mateussiilva <mateuspython3010@gmail.com>
2023-10-19 23:12:41 -0300

    Frontend funcionando haahaaha

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import PySimpleGUI as sg
 from PySimpleGUI import WIN_CLOSED,Window,Input,Button,FolderBrowse,popup_error
 
 # CONSTANTES DO SISTEMA
-SIZE_WINDOW =760,480
+SIZE_WINDOW =760,780
 
 
 layout = [
-    [sg.Frame("",layout=[
+    [sg.Frame("Manipulação de Arquivos",layout=[
         [   
             sg.Text("Selecione a pasta de origem"),
             Input(key="-INP_HTMLS-"),
@@ -21,17 +21,21 @@ layout = [
             ]
         ],
     )],
-    [Button("Gerar json",key="-CREATE_JSON_FILES")]
+    [
+        sg.Multiline(disabled=True,size=(100,40),key="-OUTPUT-")
+    ],
+    [Button("Gerar json",key="-CREATE_JSON_FILES"),Button("Carregar Arquivo txt",key="-LOAD_FILE_TXT-")]
 ]
 
 
-window = Window("Gerenciador de LOG",layout,size=SIZE_WINDOW,relative_location=(800,0))
 
+window = Window("Gerenciador de LOG",layout,size=SIZE_WINDOW,relative_location=(800,0),resizable=True)
 while 1:
     events,values = window.read()
-    FOLDER_FILES_HTMLS = values.get("-INP_HTMLS-")
-    FOLDER_FILES_JSONS = values.get("-INP_JSONS-")
+    
     if events == "-CREATE_JSON_FILES":
+        FOLDER_FILES_HTMLS = values.get("-INP_HTMLS-")
+        FOLDER_FILES_JSONS = values.get("-INP_JSONS-")
         if FOLDER_FILES_HTMLS is not None and FOLDER_FILES_JSONS:
             for arquivos in os.listdir(FOLDER_FILES_HTMLS):
                 print(arquivos)
@@ -40,7 +44,12 @@ while 1:
             popup_error(
                 "Selecione as pastas de ORIGEM e de DESTINO",title="ERROR AO SELECIONAR AS PASTAS")
             break
-    
+    if events == "-LOAD_FILE_TXT-":
+        arquivo = "arquivo_.txt"
+        with open(arquivo,"r") as file:
+            # for linha in file.readlines():
+                # print(linha)
+            window["-OUTPUT-"].update(file.read())
     if events == WIN_CLOSED:
         break
     
