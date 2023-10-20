1f2f64dec78c35b2c16b6a6253214d8836ae433f
mateussiilva <mateuspython3010@gmail.com>
2023-10-19 22:47:38 -0300

    Construindo a interface

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
+import os
+
+
 import PySimpleGUI as sg
-from PySimpleGUI import WIN_CLOSED,Window,Input,Button,FolderBrowse
+from PySimpleGUI import WIN_CLOSED,Window,Input,Button,FolderBrowse,popup_error
 
-
+# CONSTANTES DO SISTEMA
+SIZE_WINDOW = 800,480
 
 
 layout = [
-    [FolderBrowse("Pasta de Origem")]
+    [sg.Frame("",layout=[
+        [   
+            sg.Text("Selecione a pasta de origem"),
+            Input(key="-INP_HTMLS-"),
+            FolderBrowse("Pasta de Origem",key="-PATH_SOURCE_HTMLS-")
+        ],[
+            sg.Text("Selecione a pasta de destino"),
+            Input(key="-INP_JSONS-"),
+            FolderBrowse("Pasta de Origem",key="-PATH_DESTINO_JSONS-")
+            ]
+        ],
+    )],
+    [Button("Gerar json",key="-CREATE_JSON_FILES")]
 ]
 
 
-window = Window("Gerenciador de LOG",layout)
+window = Window("Gerenciador de LOG",layout,size=SIZE_WINDOW)
 
 while 1:
     events,values = window.read()
+    FOLDER_FILES_HTMLS = values.get("-INP_HTMLS-")
+    FOLDER_FILES_JSONS = values.get("-INP_JSONS-")
+    if events == "-CREATE_JSON_FILES":
+        if FOLDER_FILES_HTMLS is not None and FOLDER_FILES_JSONS:
+            for arquivos in os.listdir(FOLDER_FILES_HTMLS):
+                print(arquivos)
+            window.close()
+        else:
+            popup_error(
+                "Selecione as pastas de ORIGEM e de DESTINO",title="ERROR AO SELECIONAR AS PASTAS")
+            break
     
     if events == WIN_CLOSED:
         break
