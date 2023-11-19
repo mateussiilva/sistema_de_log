c99fb6e07e4b09e7b6e7e55ca694d6a33e6675b2
mateussiilva <mateuspython3010doo@gmail.com>
2023-11-19 14:49:04 -0300

    Começando a criar as regras de separação dos dados

--- teste.py
+++ teste.py
@@ -1,57 +1,42 @@
-# from pprint import pprint as print
+from pprint import pprint as print
+import json
 
 impressao = {
-        "IMPRESSORA": "AC-1904 W",
-        "PORTA": "FILE:",
         "ARQUIVO": "Z:\\das minys\\CORRIDOS VERAO 2023\\ESTAPAS CONFERIDAS\\D201.tif",
-        "TAMANHO DO ARQUIVO": "272461.8k",
-        "AUTOR": "User",
-        "TIPO DE TRABALHO": "Trabalho de Impress\u00e3o",
-        "TIPO": "Arquivo TIFF",
-        "AP\u00d3S A SA\u00cdDA": "Manter",
         "DIMENS\u00c3O": "150.0 x 100.0cm",
-        "RESOLU\u00c7\u00c3O": "360.00 x 1800.00",
-        "MODO DE CORES": "CMYK (2 bits)",
-        "TIPO DE DIFUS\u00c3O": "Estoc\u00e1stico avan\u00e7ado 2",
-        "MODO DE RENDERIZA\u00c7\u00c3O": "Alta Qualidade",
-        "PERFIL ICC CINZA": "GRACoL2006_Coated1v2.icc",
-        "PERFIL ICC RGB": "278523_0.icc",
-        "PERFIL ICC CMYK": "ISOcoated_v2_eci.icc",
         "PERFIL ICC DE SA\u00cdDA": "GS1904W_HIPRO_BN30GR_360X1800_3P_BZRA.icc",
-        "BITMAP EM CMYK": "Imagem",
-        "CMYK VECTOR": "Colorim\u00e9trico Relativo",
-        "CMYK TEXT": "Colorim\u00e9trico Relativo",
-        "CMYK GRADIENT": "Colorim\u00e9trico Relativo",
-        "BITMAP N\u00c3O-CMYK": "Imagem",
-        "VETOR N\u00c3O-CMYK": "Colorim\u00e9trico Relativo",
-        "TEXTO N\u00c3O-CMYK": "Colorim\u00e9trico Relativo",
-        "DEGRADE N\u00c3O-CMYK": "Colorim\u00e9trico Relativo",
-        "CORRE\u00c7\u00c3O DE CORES": "OK",
-        "USAR O PERFIL ICC EMBUTIDO PARA CMYK": "N\u00e3o",
-        "USAR O PERFIL ICC EMBUTIDO PARA RGB": "N\u00e3o",
-        "USAR O PERFIL ICC EMBUTIDO PARA CINZA": "N\u00e3o",
-        "UTILIZAR REMAPEAMENTO DE CORES SPOT": "N\u00e3o",
         "QUANTIDADE DE C\u00d3PIAS": "25",
-        "N\u00daMERO DO PAINEL": "1",
-        "N\u00daMERO DE P\u00c1GINAS": "1",
-        "ENVIAR DATA": "9/1/2023 6:27 AM",
-        "SOBREPOSI\u00c7\u00c3O": "0.500cm",
-        "IN\u00cdCIO, DATA E HORA DO RIP": "06:27:34 01/09/2023",
-        "TEMPO DE PREPARA\u00c7\u00c3O DO TRABALHO": "0.000000 sec",
-        "INFORMA\u00c7\u00d5ES": "O trabalho foi executado com sucesso",
-        "FINALIZA\u00c7\u00c3O, DATA E HORA DO RIP": "06:28:00 01/09/2023",
-        "DURA\u00c7\u00c3O DO RIP": "26 seg"
+        "IN\u00cdCIO, DATA E HORA DO RIP": "06:27:34 01/09/2023"
     }
 
-CHAVES = (
-    'ARQUIVO',
-    'DIMENSÃO',
-    'INÍCIO, DATA E HORA DO RIP',
-    'PERFIL ICC DE SAÍDA',
-    'QUANTIDADE DE CÓPIAS',
-)
 
-for chave in impressao:
-    if chave in CHAVES:
-        print(impressao[chave])
-# print(impressao)
+def limpar_dimensao(texto):
+    return float(texto.replace("cm","").split(" x ")[1])
+
+
+with open("01 09 23.json","r") as file:
+    dados = json.load(file)
+
+impressoes_moda = []
+impressao_tactel = []
+impressoes_papel = []
+
+soma_moda = soma_painel = soma_papel = 0
+for impressao,dado in dados.items():
+    nome = dado.get("ARQUIVO").lower()
+    # print(nome)
+    if "das minys" in nome:
+        quantidade_copias = int(dado.get("QUANTIDADE DE C\u00d3PIAS"))
+        dimensao = limpar_dimensao(dado.get("DIMENS\u00c3O"))
+        metros = (quantidade_copias * dimensao) // 100
+        soma_moda += metros
+        
+    elif "tactel" in nome or "malha" in nome:
+        quantidade_copias = int(dado.get("QUANTIDADE DE C\u00d3PIAS"))
+        dimensao = limpar_dimensao(dado.get("DIMENS\u00c3O"))
+        metros = (quantidade_copias * dimensao) // 100
+        soma_painel += metros 
+
+
+print(f"TOTAL DE IMPRESSÃO DE MODA: {soma_moda}")
+print(f"TOTAL DE IMPRESSÃO DE PAINEL: {soma_painel}")
