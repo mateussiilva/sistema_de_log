50509d2c578c9a1393aefe4c68d256185b03a046
mateussiilva <mateuspython3010@gmail.com>
2023-12-10 20:44:18 -0300

    Refatorando toda a aplicação

---
+++ Json/__init__.py
@@ -0,0 +1,16 @@
+from json import load,dump
+
+
+
+class PyJson:
+    def __init__(self,nome_arquivo_json:str,codificacao:str=None) -> None:
+        self.arquivo_json = nome_arquivo_json
+        self.encode = codificacao
+        pass
+    
+    def  ler_arquivo_json(self):
+        pass
+    
+    def escrever_json(self):
+        pass
+    
