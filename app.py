491cfac81f5cba161742b948cedb52b7315dcd0a
mateussiilva <mateuspython3010@gmail.com>
2024-04-01 05:36:53 -0300

    Melhorando a interface somar os metros semanais

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from pyjson import PyJson
-from utils import limpar_dimensao
+from utils import limpar_dimensao,pegar_arquivos_html
 from pprint import pprint as pprint
 import time
+import os 
 
 
 
-FILE = r"/media/mateus/D395-E345/1904/14 03 24.json"
-pyjson =  PyJson(FILE)
+
+
+PATH = "/media/mateus/D395-E345/1904/"
 tactel = malha = 0
-for imp in pyjson.ler_json().values():
-    nome = imp.get("ARQUIVO").lower()
-    if "tactel" in nome or "tactl" in nome:
-        dimensoes = imp.get("DIMENS\u00c3O")
-        metro = limpar_dimensao(dimensoes)
-        tactel += metro
-    if "malha" in nome:
-        dimensoes = imp.get("DIMENS\u00c3O")
-        metro = limpar_dimensao(dimensoes)
-        malha += metro
-    # print()
+for arquivo in os.listdir(PATH):
+    arquivo = os.path.join(PATH,arquivo)
+    pyjson =  PyJson(arquivo)
+   
+
+    for imp in pyjson.ler_json().values():
+        nome = imp.get("ARQUIVO").lower()
+        if "tactel" in nome or "tactl" in nome:
+            dimensoes = imp.get("DIMENS\u00c3O")
+            metro = limpar_dimensao(dimensoes)
+            tactel += metro
+        if "malha" in nome:
+            dimensoes = imp.get("DIMENS\u00c3O")
+            metro = limpar_dimensao(dimensoes)
+            malha += metro
+        # print()
     
-print(tactel // 100)
-print(malha // 100)
+    print(tactel // 100)
+    print(malha // 100)
+    print("---------------")
