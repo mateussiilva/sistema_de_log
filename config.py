92c3f409e6fa0ce70c3f5bc1b1237a0d98525d91
mateussiilva <mateuspython3010@gmail.com>
2024-07-30 19:47:23 -0300

    Python fil de configuração

---
+++ config.py
@@ -0,0 +1,20 @@
+
+
+PLOTTERS = {
+    "mutoh": "1604",
+    "prisamjet": "1602",
+    "prismatex": "1904",
+    "solvente": "SY160",
+}
+
+
+CHAVES = (
+    'ARQUIVO',
+    'DIMENSÃO',
+    'INÍCIO, DATA E HORA DO RIP',
+    'PERFIL ICC DE SAÍDA',
+    'QUANTIDADE DE CÓPIAS',
+)
+
+
+SIZE = (900, 700)
